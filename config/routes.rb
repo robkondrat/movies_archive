ac229a7f34c9aa5eb04d512cc58a3c3e6ae7a6e3
EatYourSoup7 <r.kondratowicz1@gmail.com>
2020-05-12 12:07:15 -0500

    directors views and partial movies views

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,9 @@ Rails.application.routes.draw do
   # namespace :api do
   #   get "/photos" => "photos#index"
   # end
+  namespace :api do
+    resources :directors
+    resources :movies
+    root 'directors#new'
+  end
 end

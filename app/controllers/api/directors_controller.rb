fe59722a7d09a75dc9708fea532220329f8d22f8
EatYourSoup7 <r.kondratowicz1@gmail.com>
2020-05-12 11:39:36 -0500

    director and movie controllers setup

--- app/controllers/api/directors_controller.rb
+++ app/controllers/api/directors_controller.rb
@@ -1,2 +1,44 @@
 class Api::DirectorsController < ApplicationController
+  before_action :set_director, only: [:show, :edit, :update, :destroy]
+  require 'csv'
+
+  def index
+    @directors = Director.all
+  end
+
+  def create
+    csv_path = params["director"]["upload"].path 
+    AddDirectorWorker.new.perform(csv_path)
+    redirect_to root_path
+  end
+
+  def update
+    respond_to do |format|
+      if @director.update(director_params)
+        format.html { redirect_to @director, notice: 'Director was successfully updated.'}
+        format.json { render :show, status: :ok, location: @director }
+      else
+        format.html { render :edit }
+        format.json { render json: @director.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @director.destroy
+    respond_to do |format|
+      format.html { redirect_to directors_url, notice: 'Director was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    def set_director
+      @director = Director.find(params[:id])
+    end
+
+    def director_params
+      params.fetch(:cert, {})
+    end
+
 end
